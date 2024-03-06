08bfd7b7430b660129419e021f514fcecc3e0d8a
Luca Piras <lucapiras13@protonmail.com>
2024-03-06 04:01:23 +0100

    snapshot

---
+++ capitoli/pdf-date.sh
@@ -0,0 +1,2 @@
+mv Piras_Luca_Tesi_Informatica_Forense.odt "Piras_Luca_Tesi_Informatica_Forense_$(date +%Y.%m.%d).odt"
+mv Piras_Luca_Tesi_Informatica_Forense.pdf "Piras_Luca_Tesi_Informatica_Forense_$(date +%Y.%m.%d).pdf"
